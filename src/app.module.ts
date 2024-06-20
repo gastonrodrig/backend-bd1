1704a15b83ea1e1022f3ed8993fdefb9d36b11b6
gastonrodrig04 <202111913@urp.edu.pe>
2024-06-20 10:31:06 -0500

    1er commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      password: process.env.DATABASE_PASSWORD,
+      username: process.env.DATABASE_USERNAME,
+      entities: [__dirname + '/*/.entity{.ts,.js}'],
+      database: process.env.DATABASE_NAME,
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
